839e6b4083afc266c53ffc0f2aa5583bed2a56b8
Salman Khan <salmantechhub336@gmail.com>
2026-04-21 13:23:17 +0500

    changes

--- project_01/src/App.jsx
+++ project_01/src/App.jsx
@@ -1,4 +1,13 @@
-function App(){
-  return 'Salman Khan'
-}
-export default App
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Salman Khan</h1>
+      <h2> Frontend Developer</h2>
+      <h3>i am a passionate frontend developer with a strong foundation in Reactjs and nodejs</h3>
+    </div>
+  );
+};
+
+export default App;

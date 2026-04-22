4b6d6b81ecb8aab8d8d4e50d0296e1722ad6111c
Salman Khan <salmantechhub336@gmail.com>
2026-04-22 23:48:59 +0500

    card and style the card

--- project_02/src/App.jsx
+++ project_02/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div className="card">
+        <h1>Salman Khan</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut doloremque
+          ipsa nesciunt veritatis repudiandae iure unde ducimus et, eius quae
+          voluptatum natus repellat rem ea ipsam possimus sit soluta distinctio.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;

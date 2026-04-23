8376e14f1970c5f40a481459f7daa686ef569441
Salman Khan <salmantechhub336@gmail.com>
2026-04-23 19:19:02 +0500

    changes

--- project_02/src/App.jsx
+++ project_02/src/App.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import Card from "./components/Card";
+import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
-  const user = 'Salman Khan'
-  const age = 22
   return (
-    <div>
-      <div className="card">
-        <h1>hello guys i am {user}</h1>
-        <h2>i am {age} years old</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-      </div>
-
+    <div> 
       <Card />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- project_02/src/components/Card.jsx
+++ project_02/src/components/Card.jsx
@@ -1,5 +1,12 @@
 function Card() {
-  return <h2>Salman Khan</h2>;
+  return <div className="card">
+    <h1> hello guys I am Salman KHan</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum beatae quis
+      expedita numquam obcaecati quos eaque porro ullam, optio explicabo sint
+      officiis dolor, dicta nesciunt atque perspiciatis error, repellat vero!
+    </p>
+  </div>;
 }
 
 export default Card;

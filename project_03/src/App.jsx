0b786d750c9cc7746bacb59cbb07106661093cbe
Salman Khan <salmantechhub336@gmail.com>
2026-04-24 01:36:16 +0500

    changes

--- project_03/src/App.jsx
+++ project_03/src/App.jsx
@@ -4,8 +4,9 @@ import Card from "./components/Card";
 const App = () => {
   return (
     <div className="parent">
-      <Card user = 'Salman Khan' age = {22}/>
-      <Card user = 'Sharukh Khan' age = {25}/>
+      <Card user = 'Salman Khan' age = {22} img = "https://images.unsplash.com/photo-1654110455429-cf322b40a906?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user = 'Sharukh Khan' age = {25} img = "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user = 'Katrina Kaif' age = {30} img = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
 
     </div>
   );
--- project_03/src/components/Card.jsx
+++ project_03/src/components/Card.jsx
@@ -5,10 +5,7 @@ const Card = (props) => {
   
   return (
      <div className="card">
-        <img
-          src="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="salman khan"
-        />
+        <img src={props.img} alt="" />
         <h1>{props.user}</h1>
         <h2>Age: {props.age}</h2>
         <p>

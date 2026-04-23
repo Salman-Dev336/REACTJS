4595a4ae18651f988df1ddf812ecb7ec2ed79bc4
Salman Khan <salmantechhub336@gmail.com>
2026-04-23 20:58:50 +0500

    changes

--- project_03/src/App.jsx
+++ project_03/src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <div className='card'>
+    <div className="parent">
+      <div className="card">
+        <img
+          src="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="salman khan"
+        />
         <h1>Salman Khan</h1>
+        <p>
+          Salman Khan is an Indian film actor, producer, and television
+          personality. He is one of the most popular and influential actors in
+          the Indian film industry, known for his charismatic screen presence
+          and philanthropic work.
+        </p>
+        <button>View Profile</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

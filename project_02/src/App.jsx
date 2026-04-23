561d0c7f6b34e50f209fc71b4f4963d3787ab109
Salman Khan <salmantechhub336@gmail.com>
2026-04-23 19:40:26 +0500

    navbar

--- project_02/src/App.jsx
+++ project_02/src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Card from './components/Card'
+import React from "react"
+import Card from "./components/Card"
+import Navbar from "./components/Navbar"
+
 
 const App = () => {
   return (
-    <div> 
+    <div>
       <Card />
-    </div>
-  )
-}
+      <Navbar />
 
-export default App
+    </div>
+  );
+};
+
+export default App;
--- project_02/src/components/Card.jsx
+++ project_02/src/components/Card.jsx
@@ -1,12 +1,18 @@
-function Card() {
-  return <div className="card">
-    <h1> hello guys I am Salman KHan</h1>
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className="card">
+    <h1>Salman KHan</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum beatae quis
       expedita numquam obcaecati quos eaque porro ullam, optio explicabo sint
       officiis dolor, dicta nesciunt atque perspiciatis error, repellat vero!
     </p>
-  </div>;
+  </div>
+    </div>
+  )
 }
 
-export default Card;
+export default card

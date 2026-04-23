dd8ccb84e822dc3ce113fbe1d5349ec007b0063b
Salman Khan <salmantechhub336@gmail.com>
2026-04-23 19:03:26 +0500

    changes

--- project_02/src/App.jsx
+++ project_02/src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Card from "./components/Card";
 
 const App = () => {
+  const user = 'Salman Khan'
   return (
     <div>
       <div className="card">
-        <h1>Salman Khan</h1>
+        <h1>hello guys i am {user}</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>

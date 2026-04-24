309b6ab8816f0545e938e4de2943064e133cfd6f
Salman Khan <salmantechhub336@gmail.com>
2026-04-25 00:24:38 +0500

    changes

--- project_05/src/App.jsx
+++ project_05/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Button from './components/Button'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Button />
+      <Header />
+      <Navbar />
+    </div>
   )
 }
 
---
+++ project_05/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='this is navbar'>Navbar</div>
+  )
+}
+
+export default Navbar

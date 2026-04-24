1070e2f6ade56d0ff97be5d2be4b9dec4624bdeb
Salman Khan <salmantechhub336@gmail.com>
2026-04-24 15:58:59 +0500

    card done

--- project_04/src/App.jsx
+++ project_04/src/App.jsx
@@ -1,38 +1,16 @@
 import React from "react";
-import { Bookmark } from "lucide-react";
+import Card from "./components/Card";
 
 const App = () => {
   return (
     <div className="parent">
-      <div className="card">
-        <div className="top">
-          <img
-            src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg?w=768"
-            alt=""
-          />
-          <button>
-            Save
-            <Bookmark size={14} />
-          </button>
-        </div>
-        <div className="center">
-          <h3>
-            Amazon <span>5 days ago</span>
-          </h3>
-          <h2>Senior UI/UX Designer</h2>
-          <div>
-            <h4>Part-time</h4>
-            <h4>Senior level</h4>
-          </div>
-        </div>
-        <div className="bottom">
-          <div>
-            <h3>$120/hr</h3>
-            <p>kpk, Pakistan</p>
-          </div>
-          <button>Apply Now</button>
-        </div>
-      </div>
+     < Card />
+     < Card />
+     < Card />
+
+    
+
+
     </div>
   );
 };

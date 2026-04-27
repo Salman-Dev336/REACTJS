f74107dc56d2bd801147c18fc337b5605ca4e0a4
Salman Khan <salmantechhub336@gmail.com>
2026-04-27 21:18:49 +0500

    changes

--- project_08/src/App.jsx
+++ project_08/src/App.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  function btnClicked() {
+    console.log("button is clicked");
+  }
 
-  function btnClicked(){
-      console.log("button is clicked");
-    }
+  // function mouseEnter() {
+  //   console.log("mouse enter");
+  // }
 
-    function mouseEnter(){
-      console.log("mouse enter");
-    }
-
-    function mouseLeave (){
-            console.log("mouseLeave");
-    }
+  function mouseLeave() {
+    console.log("mouseLeave");
+  }
 
   return (
-    
     <div>
       <h1>hello salman</h1>
-      <button  onMouseEnter={mouseEnter} onDoubleClick={btnClicked}
-      onMouseLeave={mouseLeave}>click here  </button>
+      <button onClick={function(){
+        console.log("btn Clicked ");
+        
+      }}>click here </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

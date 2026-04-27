b1d31494408ab030c1888c0ebf0cca583ed39590
Salman Khan <salmantechhub336@gmail.com>
2026-04-27 21:13:43 +0500

    changes

--- project_08/src/App.jsx
+++ project_08/src/App.jsx
@@ -5,11 +5,21 @@ const App = () => {
   function btnClicked(){
       console.log("button is clicked");
     }
+
+    function mouseEnter(){
+      console.log("mouse enter");
+    }
+
+    function mouseLeave (){
+            console.log("mouseLeave");
+    }
+
   return (
     
     <div>
       <h1>hello salman</h1>
-      <button onClick={btnClicked}>click here  </button>
+      <button  onMouseEnter={mouseEnter} onDoubleClick={btnClicked}
+      onMouseLeave={mouseLeave}>click here  </button>
     </div>
   )
 }

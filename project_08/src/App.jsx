d59e8e4a9df64ebb05337988acbdea3c89c1fc40
Salman Khan <salmantechhub336@gmail.com>
2026-04-28 18:11:19 +0500

    changes

--- project_08/src/App.jsx
+++ project_08/src/App.jsx
@@ -1,36 +1,14 @@
 import React from "react";
 
 const App = () => {
-  function btnClicked() {
-    console.log("button is clicked");
-  }
-
-  // function mouseEnter() {
-  //   console.log("mouse enter");
-  // }
-
-  function mouseLeave() {
-    console.log("mouseLeave");
-  }
-
-  function inputChanging(value) {
-    console.log(value);
-  }
-
   return (
     <div>
-      <input onChange={function(element){
-        inputChanging(element.target.value);
-        // console.log(element.target.value);
+      <div onMouseMove={() => {
+        console.log("hehehhehehhehehe");
 
-        
-      }} type="text" placeholder="enter name " />
-
-      {/* <h1>hello salman</h1>
-      <button onClick={()=>{
-        console.log("btn Clicked ");
-        
-      }}>click here </button> */}
+      }} className="box">
+        salman khan
+      </div>
     </div>
   );
 };

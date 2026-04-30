b811638d257da85deab2249c16aebaa682e66c12
Salman Khan <salmantechhub336@gmail.com>
2026-05-01 03:45:08 +0500

    changes

--- project_09/src/App.jsx
+++ project_09/src/App.jsx
@@ -1,39 +1,60 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [num, setnum] = useState(20);
-  const [username, setusername] = useState("Salman");
-  const [users, setUsers] = useState([10,20,30]);
+  const [num, setnum] = useState(0);
 
-  function changeNum() {
-    setnum(100);
-    setusername("Sharukh Khan");
-    setUsers([40,50,60]);
-    console.log(num);
-    console.log(username);
-    console.log(users);
-    
-    
-    
+  function increament() {
+    setnum(num + 1);
+  }
+  function decreament() {
+    setnum(num - 1);
   }
 
-  // let a = 20;
-
-  // function changeA(){
-  //     console.log(a);
-  //     a = 30;
-  //     console.log(a);
-  //  }
   return (
     <div>
-      <h1>value of num is {num}</h1>
-      <br />
-      <h1>my name is {username}</h1>
-      <br />
-      <h1>my roll no is {users}</h1>
-      <button onClick={changeNum}>click me</button>
+      <h1>{num}</h1>
+      <button onClick={increament}>increase</button>
+      <button onClick={decreament}>decrease</button>
     </div>
   );
 };
 
 export default App;
+
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [num, setnum] = useState(20);
+//   const [username, setusername] = useState("Salman");
+//   const [users, setUsers] = useState([10,20,30]);
+
+//   function changeNum() {
+//     setnum(100);
+//     setusername("Sharukh Khan");
+//     setUsers([40,50,60]);
+//     console.log(num);
+//     console.log(username);
+//     console.log(users);
+
+//   }
+
+//   // let a = 20;
+
+//   // function changeA(){
+//   //     console.log(a);
+//   //     a = 30;
+//   //     console.log(a);
+//   //  }
+//   return (
+//     <div>
+//       <h1>value of num is {num}</h1>
+//       <br />
+//       <h1>my name is {username}</h1>
+//       <br />
+//       <h1>my roll no is {users}</h1>
+//       <button onClick={changeNum}>click me</button>
+//     </div>
+//   );
+// };
+
+// export default App;

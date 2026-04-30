aa4be2c0f6f85da0bf207597be7efe6460a599ca
Salman Khan <salmantechhub336@gmail.com>
2026-05-01 01:49:36 +0500

    changes

--- project_09/src/App.jsx
+++ project_09/src/App.jsx
@@ -1,34 +1,33 @@
- import React, { useState } from 'react'
- 
- const App = () => {
+import React, { useState } from "react";
 
-    const [num, setnum] = useState(20);
-    const [username, setusername] = useState("Salman");
+const App = () => {
+  const [num, setnum] = useState(20);
+  const [username, setusername] = useState("Salman");
 
-    function changeNum(){
-        
-        setnum(100)        
-        setusername("Sharukh Khan")
-        
+  function changeNum() {
+    setnum(100);
+    setusername("Sharukh Khan");
+    console.log(num);
+    console.log(username);
+    
+    
+  }
 
-    }
+  // let a = 20;
 
-   
+  // function changeA(){
+  //     console.log(a);
+  //     a = 30;
+  //     console.log(a);
+  //  }
+  return (
+    <div>
+      <h1>value of num is {num}</h1>
+      <br />
+      <h1>my name is {username}</h1>
+      <button onClick={changeNum}>click me</button>
+    </div>
+  );
+};
 
-    // let a = 20;
-
-    // function changeA(){
-    //     console.log(a);
-    //     a = 30;
-    //     console.log(a);
-//  }
-   return (
-     <div>
-        <h1>value of num is {num}</h1><br />
-        <h1>my name is {username}</h1>        
-        <button onClick={changeNum}>click me</button>
-     </div>
-   )
- }
- 
- export default App
+export default App;

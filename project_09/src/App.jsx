575ad593d54b2dd382ee3a8c49465c406be47c1d
Salman Khan <salmantechhub336@gmail.com>
2026-05-01 01:37:57 +0500

    changes

--- project_09/src/App.jsx
+++ project_09/src/App.jsx
@@ -1,18 +1,27 @@
- import React from 'react'
+ import React, { useState } from 'react'
  
  const App = () => {
-    let a = 20;
 
-    function changeA(){
-        console.log(a);
-        a = 30;
-        console.log(a);
-        
+    const [num, setnum] = useState(20);
+
+    function changeNum(){
+        setnum(100)        
+
     }
+
+   
+
+    // let a = 20;
+
+    // function changeA(){
+    //     console.log(a);
+    //     a = 30;
+    //     console.log(a);
+//  }
    return (
      <div>
-        <h1>{a}</h1>
-        <button onClick={changeA}>click me</button>
+        <h1>value of num is {num}</h1>
+        <button onClick={changeNum}>click me</button>
      </div>
    )
  }

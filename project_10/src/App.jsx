5a4655acce6dd8733624815dc6a6b4b031c5a128
Salman Khan <salmantechhub336@gmail.com>
2026-05-02 01:41:58 +0500

    changes

---
+++ project_10/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text"/>
+        <input type="text" />
+        <button>Login</button>
+
+      </form>
+    </div>
+  )
+}
+
+export default App

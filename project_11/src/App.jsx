b37d8979c1e15f7ab6e6fdf3079f1a66438a57f1
Salman Khan <salmantechhub336@gmail.com>
2026-05-02 02:24:18 +0500

    changes

--- project_11/src/App.jsx
+++ project_11/src/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  
+  const [email, setemail] = useState('salma123@gmail.com');
 
-  const submitHandler = (e)=>{
+  const submitHandler = (e) => {
     e.preventDefault();
-    console.log('form submitted');
-    
-  }
+    console.log("form submitted");
+  };
 
   return (
     <div>
-      <form onSubmit={(e)=>{
-        submitHandler(e);
-      }}>
-        <input type="text" placeholder='Enter your email'/>
-        <input type="text" placeholder='Enter your password'/>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter your email"
+          value={email}
+          // onChange={(e) => {
+          //   console.log(e.target.value);
+          // }}
+        />
+        <input type="text" placeholder="Enter your password" />
         <button>Submit</button>
-
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

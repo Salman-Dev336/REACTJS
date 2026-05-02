22075494b91529ff85bfbb838f364a4f438802ba
Salman Khan <salmantechhub336@gmail.com>
2026-05-03 03:55:01 +0500

    changes

--- project_12/src/App.jsx
+++ project_12/src/App.jsx
@@ -3,29 +3,34 @@ import React from "react";
 const App = () => {
   return (
     <div className="h-screen bg-black ">
-      <form className="flex items-start gap-5 p-10 justify-between">
+      <div>
+        <nav className="h-20 flex justify-between p-10 ">
+          <h3 className="text-white font-bold text-xl font-serif">My Notes</h3>
+          <img
+            className="rotate-y-180 h-15  w-15"
+            src="https://static.vecteezy.com/system/resources/thumbnails/049/578/155/small/a-black-and-white-drawing-of-a-man-writing-png.png"
+            alt=""
+          />
+        </nav>
+      </div>
+
+      <form className="flex items-start p-10 justify-between">
         <div className="flex items-start flex-col w-1/2 gap-4 ">
           <input
             type="text"
             placeholder="Enter Notes Heading"
-            className="px-5 w-full py-2 border-2 rounded text-white"
+            className="px-5 w-full py-2 border-2 rounded text-white font-medium"
           />
-          <input
+          <textarea
             type="text"
-            className="px-5 w-full py-2 rounded border-2 h-20 text-white"
+            className="px-5 w-full py-2 rounded border-2 h-30 text-start text-white font-medium "
             placeholder="Enter details"
           />
           <button className="text-black w-full  bg-white border-2 px-5 py-2 hover:bg-blue-400 hover:text-black rounded-2xl">
             Add Note
           </button>
         </div>
-         <img
-        className="h-60"
-        src="https://pngimg.com/uploads/sticky_note/sticky_note_PNG18899.png"
-        alt=""
-      />
       </form>
-     
     </div>
   );
 };

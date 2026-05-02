6610177d16c759857581f83eaca60d9802d84b1c
Salman Khan <salmantechhub336@gmail.com>
2026-05-03 02:16:51 +0500

    changes

--- project_12/src/App.jsx
+++ project_12/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <form className="flex justify-between">
-        <input type="text" placeholder="Enter Notes Heading" />
-        <textarea placeholder="Enter details" name="" id=""></textarea>
+    <div className="h-screen bg-black">
+      <form className="flex p-10">
+        <input type="text" placeholder="Enter Notes Heading"
+        className="px-5 py-2 border-2 rounded text-white" />
+        <input type="text" className="text-white"
+        placeholder="Enter details" />
       </form>
     </div>
   );

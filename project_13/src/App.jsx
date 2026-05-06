e310d83ccdfc277a12639d053c99bd22c7d37b75
Salman Khan <salmantechhub336@gmail.com>
2026-05-07 00:28:46 +0500

    get item form local storage

--- project_13/src/App.jsx
+++ project_13/src/App.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const App = () => {
    localStorage.clear()
   localStorage.setItem('user', 'salman')
+
+  const user = localStorage.getItem('user')
+  console.log(user);
+  
  
   return (
     <div>App</div>

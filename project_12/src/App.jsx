74e952a7d5cb89d7aebb36c9a78d75e217874105
Salman Khan <salmantechhub336@gmail.com>
2026-05-06 17:25:49 +0500

    changes

--- project_12/src/App.jsx
+++ project_12/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { X } from 'lucide-react';
+import { X } from "lucide-react";
 const App = () => {
   const [title, settitle] = useState("");
   const [details, setdetails] = useState("");
@@ -58,7 +58,6 @@ const App = () => {
             value={details}
             onChange={(e) => {
               // console.log(e.target.value);
-
               setdetails(e.target.value);
             }}
           />
@@ -69,12 +68,23 @@ const App = () => {
         <div className="  p-10 lg:w-1/2 flex lg:border-l-2 border-white flex-col items-start ">
           <h1 className="text-white text-3xl font-bold">Recent Notes</h1>
           <div className="flex flex-wrap gap-5 mt-10  max-h-141.5 overflow-auto  ">
-            {task.map(function(e, index){
-              return   <div key={index} className="relative h-52 w-40 rounded-2xl py-8 px-5 bg-cover bg-[url('https://static.vecteezy.com/system/resources/thumbnails/010/793/873/small/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png')]">
-                <h2 className="absolute top-7 right-3 border-1 bg-red-500"><X/></h2>
-                <h3 className=" text-2xl font-bold leading-tight mt-5">{e.title}</h3>
-                <p className="leading-4 mt-2 font-serif text-gray-700" >{e.details}</p>
-              </div>
+            {task.map(function (e, index) {
+              return (
+                <div
+                  key={index}
+                  className="relative h-52 w-40 rounded-2xl py-8 px-5 bg-cover bg-[url('https://static.vecteezy.com/system/resources/thumbnails/010/793/873/small/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png')]"
+                >
+                  <h2 className="absolute top-7 right-3 rounded-3xl bg-red-500">
+                    <X size={16} color="#fdfcfc" strokeWidth={3} />{" "}
+                  </h2>
+                  <h3 className=" text-2xl font-bold leading-tight mt-5">
+                    {e.title}
+                  </h3>
+                  <p className="leading-4 mt-2 font-serif text-gray-700">
+                    {e.details}
+                  </p>
+                </div>
+              );
             })}
           </div>
         </div>

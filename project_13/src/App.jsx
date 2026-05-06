3b34f1daeebb136617c7ef60269c17e34a28b12b
Salman Khan <salmantechhub336@gmail.com>
2026-05-07 00:33:09 +0500

    changes

--- project_13/src/App.jsx
+++ project_13/src/App.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 const App = () => {
    localStorage.clear()
   localStorage.setItem('user', 'salman')
+  localStorage.setItem('age', 22)
+
 
   const user = localStorage.getItem('user')
+  const age = localStorage.getItem('age')
+
+  localStorage.removeItem('age')
   console.log(user);
+  console.log(age);
+  
   
  
   return (

e1cc5ed4ed3707e63ac32c75c61060913ae54fc0
Salman Khan <salmantechhub336@gmail.com>
2026-05-07 00:51:35 +0500

    local storage done

--- project_13/src/App.jsx
+++ project_13/src/App.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 
 const App = () => {
-   localStorage.clear()
-  localStorage.setItem('user', 'salman')
-  localStorage.setItem('age', 22)
 
 
-  const user = localStorage.getItem('user')
-  const age = localStorage.getItem('age')
+  const user = {
+    name: 'salman',
+    age: 22,
+    city: 'buner'
+  }
 
-  localStorage.removeItem('age')
-  console.log(user);
-  console.log(age);
+  localStorage.setItem('user', JSON.stringify(user))
+
+  const userr = JSON.parse(localStorage.getItem('user'))
+  console.log(userr);
+  
+
+
+  //  localStorage.clear()
+  // localStorage.setItem('user', 'salman')
+  // localStorage.setItem('age', 22)
+
+
+  // const user = localStorage.getItem('user')
+  // const age = localStorage.getItem('age')
+
+  // localStorage.removeItem('age')
+  // console.log(user);
+  // console.log(age);
   
   
  

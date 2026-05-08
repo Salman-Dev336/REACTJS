9b24335526c003427a193ef6e04b5f6fe6ae2c43
Salman Khan <salmantechhub336@gmail.com>
2026-05-08 23:08:44 +0500

    changes

--- project_14/src/App.jsx
+++ project_14/src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
 
   const getData = async ()=>{
-    const response= await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    // console.log('data agaya hai');
-    const data = await response.json()
-    console.log(response);
-    console.log(data);
+    // const response= await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // // console.log('data agaya hai');
+    // const data = await response.json()
+    // console.log(response);
+    // console.log(data);
     
+    // const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    // console.log(response.data);
+
+    const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/2')
+    console.log(data);
+    console.log('data agaya hai');
+    
+
+    // {data} this is destructuring
+    
+    
+
+  
     
     
   }

bd1b4c91e2baa5f7075cfa5f0aa60bf7e91b99da
Salman Khan <salmantechhub336@gmail.com>
2026-05-08 23:16:51 +0500

    changes

--- project_14/src/App.jsx
+++ project_14/src/App.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
-
-  const getData = async ()=>{
+  const [data, setdata] = useState([]);
+  const getData = async () => {
     // const response= await fetch('https://jsonplaceholder.typicode.com/todos/1')
     // // console.log('data agaya hai');
     // const data = await response.json()
     // console.log(response);
     // console.log(data);
-    
+
     // const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
     // console.log(response.data);
 
-    const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/2')
-    console.log(data);
-    console.log('data agaya hai');
-    
+    // const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/2')
+    // console.log(data);
+    // console.log('data agaya hai');
 
     // {data} this is destructuring
-    
-    
 
-  
-    
-    
-  }
+    const response = await axios.get("https://picsum.photos/v2/list");
+    // console.log(response.data);
+    setdata(response.data);
+  };
   return (
     <div>
       <button onClick={getData}>get data</button>
+      <div>
+        {data.map(function(){
+          return <h3>hello</h3>
+        })}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

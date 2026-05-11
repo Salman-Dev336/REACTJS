f4959c4a6909fd865b1900dfd0862aea64f8d7a0
Salman Khan <salmantechhub336@gmail.com>
2026-05-11 19:52:43 +0500

    changes

--- project_15/src/App.jsx
+++ project_15/src/App.jsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from "react";
 
-
 // const random = () => {
 //   const a = Math.random();
 //   console.log(a);
 // };
 
 const App = () => {
-  const [num, setnum] = useState(0)
+  const [num, setnum] = useState(0);
 
-  useEffect(function(){
-  console.log("use effect");
-  
-})
+  useEffect(function () {
+    console.log("use effect");
+  }, []);
   // random();
 
   return (
     <div>
       <h1>{num}</h1>
-      <button onClick={()=>{
-        setnum(20)
-      }}>click here</button>
+      <button
+        onClick={() => {
+          setnum(20);
+        }}
+      >
+        click here
+      </button>
     </div>
   );
 };

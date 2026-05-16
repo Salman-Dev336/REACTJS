02f5a00ee9d277c18405aa3d0b60b4667f2be7c2
Salman Khan <salmantechhub336@gmail.com>
2026-05-16 14:33:04 +0500

    changes

--- project_15/src/App.jsx
+++ project_15/src/App.jsx
@@ -4,6 +4,24 @@ import React, { useEffect, useState } from "react";
 //   console.log(a);
 // };
 const App = () => {
+
+  const [a, seta] = useState(0)
+  const [b, setb] = useState(0)
+
+
+  const aChanging = ()=>{
+        seta(a+10);
+    console.log('a ki value change hogai');
+
+    
+  }
+
+   const bChanging = ()=>{
+    setb(b+20);
+    console.log('b ki value change hogai');
+    
+    
+  }
   // const [num, setnum] = useState(0);
   // const [num2, setnum2] = useState(100);
   // useEffect(function () {
@@ -13,7 +31,14 @@ const App = () => {
   return (
     <div>
 
-      
+      <h1>A :{a}</h1>
+      <h1>B :{b}</h1>
+
+
+      <button onClick={aChanging}>change A</button>
+      <button onClick={bChanging}>change B</button>
+
+
       {/* <h1>{num}</h1>
       <h1>{num2}</h1>
       <button
